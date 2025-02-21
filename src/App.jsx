6e7f0f024f4918099886ced4cb28e653df7b8eb5
[Bharath] <155085699+bScripter@users.noreply.github.com>
2025-02-21 22:27:40 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  
+  return (
+    <>
+      <h1 className="m-10 text-xl text-bold">This is tailWindCSS Configured.</h1>
+    </>
+  )
+}
+
+export default App
