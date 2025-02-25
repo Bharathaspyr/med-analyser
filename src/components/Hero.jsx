cbbf069975d87de43cff49e1f4aa401b15c82f31
Ahalya-V-S <v.s.ahalya2005@gmail.com>
2025-02-25 18:24:04 +0530

    Updated Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,13 +6,13 @@ const Hero = () => {
   const navigate = useNavigate();
   return (
     <div className="flex-grow flex items-center justify-center text-center bg-gradient-to-r from-blue-500 via-purple-500 to-indigo-600 text-white min-h-screen pt-20 w-full">
-      <div className="max-w-2xl w-full px-4">
+      <div className="max-w-2xl w-full">
         <Typewriter
           words={[
-            { text: "AI-Powered ", className: "text-white" },
-            { text: " MedScan - AI", className: "text-yellow-400" }
+            { text: "AI  ", className: "text-white" },
+            { text: " Medical Image Analyser", className: "text-yellow-400" }
           ]}
-          className="text-4xl font-bold leading-tight drop-shadow-lg"
+          className="text-4xl font-bold drop-shadow-lg"
           smooth
         />
         <p className="mt-6 text-xl opacity-90">
